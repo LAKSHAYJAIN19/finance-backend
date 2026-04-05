7493201374790d79ff70c48df09260c5132ffa4d
LAKSHAYJAIN19 <72019776+LAKSHAYJAIN19@users.noreply.github.com>
2026-04-06 01:41:59 +0530

    Update CORS origin for frontend application

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://your-frontend.vercel.app",
+  origin: "https://finance-frontend-nine-amber.vercel.app/",
   credentials: true
 }));
 app.use(express.json());

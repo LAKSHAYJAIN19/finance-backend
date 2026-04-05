f50c3f6d0e673eb3d87a799341b7dc516f3f3039
LAKSHAYJAIN19 <72019776+LAKSHAYJAIN19@users.noreply.github.com>
2026-04-06 02:37:58 +0530

    final changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = decoded;   // ✅ THIS LINE IS CRITICAL
+        req.user = decoded;  
 
         next();
     } catch (err) {
